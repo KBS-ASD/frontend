b083a2635579506d7264e2be1b1a929dc6fe2943
Leroy Korterink <leroy@mediamonks.com>
2019-01-08 14:44:56 +0100

    Add react-chartjs and show chart with result data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { Line as LineChart } from "react-chartjs";
+import results from "./results.json";
 import "./App.scss";
+
+class MyChart extends Component {
+  constructor() {
+    super();
+
+    this.chartData = results
+      .reduce((accumulator, currentValue) => {
+        if (accumulator[currentValue.properties.MessageId] == null)
+          accumulator[currentValue.properties.MessageId] = {};
+
+        switch (currentValue.eventName) {
+          case "MessageReceived":
+            accumulator[currentValue.properties.MessageId].y = parseInt(
+              currentValue.properties.ReceivedAt,
+              10
+            );
+            break;
+
+          case "MessageSent":
+            accumulator[currentValue.properties.MessageId].x = parseInt(
+              currentValue.properties.SentAt,
+              10
+            );
+            break;
+
+          default:
+            console.info("Event name out or reach");
+        }
+
+        return accumulator;
+      }, [])
+      // Convert to timespan in milliseconds
+      .map(point => (point.y - point.x) / 10000);
+
+    // My chart options
+    this.chartOptions = {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ]
+      }
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <LineChart
+          data={{
+            labels: [...Array(this.chartData.length / 5).keys()],
+            datasets: [
+              {
+                data: this.chartData
+              }
+            ]
+          }}
+          options={this.chartOptions}
+          width="1280"
+          height="720"
+        />
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <MyChart />
       </div>
     );
   }
